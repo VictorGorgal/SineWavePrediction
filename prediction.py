3d8ba536dc4365f2b709b77b5c4ef9630d83a1d9
VictorGorgal <94933775+VictorGorgal@users.noreply.github.com>
2022-05-11 14:16:17 -0300

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,7 +1,6 @@
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from keras.models import load_model
-from keras.preprocessing.sequence import TimeseriesGenerator
 import matplotlib.pyplot as plt
 import generator
 import numpy as np
@@ -14,31 +13,27 @@ def add(array, element):
     return new
 
 
+def predict(model, data, future=144):
+    data = data.reshape(1, len(data), 1)
+    y_hat = []
+    for _ in range(future):
+        pred = model.predict(data)[0, 0]
+
+        y_hat.append(pred)
+        data = add(data, pred)
+
+    return y_hat
+
+
 x = np.linspace(0, 2, 200)
-X = generator.generate2(x, amplitude=1, frequency=2)
-y = X
-X = X.reshape((1, len(x), 1))
+y = generator.generate2(x, amplitude=1, frequency=2)
 
 model = load_model('model.h5')
 
-y_hat = []
-for i in range(X.shape[1]):
-    pred = model.predict(X)[0, 0]
-
-    y_hat.append(pred)
-    X = add(X, pred)
-
-# x = np.linspace(0, 2, 400)
-# y = generator.generate2(x, frequency=1)
-# y = y.reshape((len(y), 1))
-#
-# time_series = TimeseriesGenerator(y, y, length=200, sampling_rate=1, stride=1, batch_size=32)
-#
-# model = load_model('model.model')
-# y_hat = model.predict(time_series)
+y_hat = predict(model, y, future=200)
 
 x2 = np.linspace(1, 2, len(y_hat))
-plt.plot(x, y, label='actual')
+plt.plot(x, y.flatten(), label='actual')
 plt.plot(x2, y_hat, linestyle=':', label='predicted')
-plt.legend()
+plt.legend(loc='upper right')
 plt.show()
